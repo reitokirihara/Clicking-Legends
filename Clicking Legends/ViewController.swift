c4881b8d9036213b15f2237a847952f76e60e6d6
UCode <ucode@UCODE-TPV-THETA.fios-router.home>
2018-04-11 19:00:24 -0700

    third commit

--- Clicking Legends/ViewController.swift
+++ Clicking Legends/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
@@ -14,29 +15,60 @@ class ViewController: UIViewController {
     @IBOutlet var coinLabel: UILabel!
     @IBOutlet var hpProgressBar: UIProgressView!
     @IBOutlet var clickDamageLabel: UILabel!
-    @IBOutlet var hireLegendOneButton: UIButton!
+    @IBOutlet var damagePerSecondLabel: UILabel!
+    @IBOutlet var enemyLevelLabel: UILabel!
     
-    var hp: Float = 5
-    var maxHp: Float = 5
+    //player
+    @IBOutlet var playerLevelLabel: UILabel!
+    @IBOutlet var playerExperinceProgressLabel: UILabel!
+    @IBOutlet var playerLevelProgressBar: UIProgressView!
+    
+    //hire buttons
+    @IBOutlet var hireLegendOneButton: UIButton!
+    @IBOutlet var hireLegendTwoButton: UIButton!
+    
+    var hp: Float = 0
+    var maxHp: Float = 0
     var coins: Int = 0
-    var clickDamage: Float = 1
     var rewardCoins: Int!
-    var arenaNumber: Int = 1
+    var enemyLevel: Float = 1.0
+    var killedNumber: Float = 0
+    var playerLevel: Int = 1
+    var rewardExperience: Float!
+    var currentPlayerExperience: Float = 0
+    var maxEp: Float = 0
+    var extraEp: Float!
+    
+    //damage variables
+    var clickDamage: Float = 1
+    var damagePerSecond: Float = 0
 
-    //variables for legend one
-    var priceForLegendOne: Int = 0
-    var numberOfLegendOne: Int = 0
+    //price for legends
+    var priceForLegendOne: Int = 5
+    var priceForLegendTwo: Int = 25
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        maxEp = enemyLevel * 5
+        spawnNewEnemy(level: 1)
+        
+        playerLevelProgressBar.progress = 0
+        
         coinLabel.text = "💰 \(coins)"
         hpLabel.text = "\(hp) HP"
         clickDamageLabel.text = "Click Damage \(clickDamage)"
+        damagePerSecondLabel.text = "DPS \(damagePerSecond)"
+        playerExperinceProgressLabel.text = "0/\(maxEp)"
         
         coinLabel.sizeToFit()
         hpLabel.sizeToFit()
         clickDamageLabel.sizeToFit()
+        damagePerSecondLabel.sizeToFit()
+        playerExperinceProgressLabel.sizeToFit()
+        
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(automatedDamage), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -45,54 +77,94 @@ class ViewController: UIViewController {
     }
 
     @IBAction func tapEnemy(_ sender: AnyObject) {
-        hp -= clickDamage
-        if(hp <= 0){
-            spawnNewEnemy()
-            getRewardCoin()
-        }
-        hpProgressBar.progress = hp / maxHp
-        hpLabel.text = "\(hp) HP"
-        hpLabel.sizeToFit()
+        damage(dmg: clickDamage)
     }
     
     @IBAction func hireLegendOne(_ sender: AnyObject) {
-        if(numberOfLegendOne == 0) {
-            priceForLegendOne = 5
-            if(coins >= priceForLegendOne) {
-                numberOfLegendOne += 1
-                coins -= 5
-                clickDamage += 1
-            }
-        } else {
-            priceForLegendOne = numberOfLegendOne * 5
-            if(coins >= priceForLegendOne) {
-                coins -= priceForLegendOne
-                numberOfLegendOne += 1
-                clickDamage += 1
-            }
+      
+        if(coins >= priceForLegendOne) {
+            coins -= priceForLegendOne
+            clickDamage += 1
+            priceForLegendOne += 5
         }
         
         clickDamageLabel.text = "Click Damage \(clickDamage)"
         clickDamageLabel.sizeToFit()
         
-        hireLegendOneButton.setTitle("Hire \(priceForLegendOne)", for: .normal)
+        hireLegendOneButton.setTitle("Hire     💰\(priceForLegendOne)", for: .normal)
         hireLegendOneButton.sizeToFit()
-        
-        coinLabel.text = "💰 \(coins)"
-        coinLabel.sizeToFit()
     
+        changeCoinLabel()
+        
     }
     
-    func spawnNewEnemy(){
-        hp = maxHp + 50
+    @IBAction func hireLegendTwo(_ sender: AnyObject) {
+        
+        if(coins >= priceForLegendTwo) {
+            coins -= priceForLegendTwo
+            damagePerSecond += 20
+            priceForLegendTwo += 25
+        }
+        
+        damagePerSecondLabel.text = "DPS \(damagePerSecond)"
+        damagePerSecondLabel.sizeToFit()
+        
+        hireLegendTwoButton.setTitle("Hire     💰\(priceForLegendTwo)", for: .normal)
+        hireLegendTwoButton.sizeToFit()
+        
+        changeCoinLabel()
+    }
+    
+    func spawnNewEnemy(level: Float){
+        hp = level * 5
         maxHp = hp
+        rewardExperience = enemyLevel
+        currentPlayerExperience += rewardExperience
+        if(currentPlayerExperience >= maxEp) {
+            extraEp = currentPlayerExperience - maxEp
+            currentPlayerExperience = 0
+            playerLevel += 1
+            currentPlayerExperience += extraEp
+        }
+        playerLevelProgressBar.progress = currentPlayerExperience/maxEp
+        playerExperinceProgressLabel.text = "\(currentPlayerExperience)/\(maxEp)"
     }
     
     func getRewardCoin(){
-        rewardCoins = arenaNumber * 50
+        rewardCoins = Int(enemyLevel)
         coins += rewardCoins
         coinLabel.text = "💰 \(coins)"
         coinLabel.sizeToFit()
+    }
+    
+    func changeCoinLabel() {
+        coinLabel.text = "💰 \(coins)"
+        coinLabel.sizeToFit()
+    }
+    
+    func damage(dmg: Float){
+        hp -= dmg
+        
+        if(hp <= 0){
+            killedNumber += 1
+
+            if (killedNumber >= enemyLevel * 5) {
+                spawnNewEnemy(level: enemyLevel + 1)
+                enemyLevel += 1
+                getRewardCoin()
+            } else {
+                spawnNewEnemy(level: enemyLevel)
+                getRewardCoin()
+            }
+        }
+        hpProgressBar.progress = hp / maxHp
+        hpLabel.text = "\(hp) HP"
+        hpLabel.sizeToFit()
+    }
+   
+    @objc
+    func automatedDamage(_ timer: Timer){
+        damage(dmg: damagePerSecond)
     }
 }
 
